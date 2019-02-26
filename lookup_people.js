e38db1abe9b03c843650acc37ca94ee4c765d84c
Ken Thooptong <ken.thooptong@gmail.com>
2019-02-26 18:10:14 +0000

    Return strings with data, but dates haven't been converted

--- lookup_people.js
+++ lookup_people.js
@@ -19,7 +19,14 @@ client.connect((err) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows);
+    console.log("Seaching...");
+    console.log(`Found ${result.rows.length} person(s) by the name of '${celebrity}': `);
+    SELECT CAST("celebrity.birthdate" AS DATE);
+
+    result.rows.forEach(function(celeb){
+      console.log(`${celeb.first_name} ${celeb.last_name}, born '${celeb.birthdate}'`)
+    });
+
     client.end();
   });
 });
