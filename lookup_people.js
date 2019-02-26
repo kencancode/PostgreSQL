9adeef40b1e747665f78525f49dfb6022f75eb72
Ken Thooptong <ken.thooptong@gmail.com>
2019-02-26 16:52:40 +0000

    Connect to PostgreSQL successfully without refactoring

---
+++ lookup_people.js
@@ -0,0 +1,29 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const celebrity = process.argv.slice(2)[0];
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query(`SELECT * FROM famous_people WHERE first_name = $1::text`,[celebrity], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows);
+    client.end();
+  });
+});
+
+
+//$1::text`,[celebrity] for security
+//instead of ${'celebrity'}
